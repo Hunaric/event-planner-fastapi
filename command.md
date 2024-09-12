9196908f27abcf97bce3b3e57133dbf6bcb3a8d9
Hunaric <nathanaelfangnon@gmail.com>
2024-09-12 09:51:56 +0100

    Facing bug

---
+++ command.md
@@ -0,0 +1,4 @@
+Invoke-RestMethod -Uri 'http://127.0.0.1:8000/event/new' `
+  -Method Post `
+  -Headers @{Accept = 'application/json'; 'Content-Type' = 'application/json'} `
+  -Body '{"title": "FastAPI Book Launch", "image": "https://linktomyimage.com/image.png", "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts", "tags": ["python", "fastapi", "book", "launch"], "location": "Google Meet"}'
--- models/user.py
+++ models/user.py
@@ -1,17 +1,23 @@
-from pydantic import BaseModel, EmailStr
+from beanie import Document, Link
 from typing import Optional, List
-from models.event import Event
 
-class User(BaseModel):
+from models.event import Event
+from pydantic import BaseModel, EmailStr
+
+class User(Document):
     email: EmailStr
     password: str
+    events: Optional[List[Link[Event]]]
 
+    class Settings:
+        name = "user"
 
     class Config:
         json_schema_extra = {
             "example": {
                 "email": "fastapi@learn.com",
                 "password": "strong!!!",
+                "events": [],
             }
         }
 
