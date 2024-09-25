64782abe6b2e1af4e266de85b952675b97ec0447
Hunaric <nathanaelfangnon@gmail.com>
2024-09-25 12:31:43 +0100

    Start deploying with Docker

--- main.py
+++ main.py
@@ -8,20 +8,24 @@ from database.connection import Settings
 from routes.user import user_router
 from routes.event import event_router
 
-# register origins
 
-origins = ["*"]
+app = FastAPI()
 
 settings = Settings()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await settings.initialize_database()
-    yield
+# register origins
+origins = ["*"]
 
-app = FastAPI(lifespan=lifespan)
 
-app.middleware(
+# Update app definition
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     await settings.initialize_database()
+#     yield
+
+# app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
@@ -29,15 +33,19 @@ app.middleware(
     allow_headers=["*"],
 )
 
+# Register routes
+
+app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
+
+app.on_event("startup")
+async def init_db():
+    await settings.initialize_database()
 
 @app.get("/")
 async def home():
     return RedirectResponse(url="/event/")
 
-# Register routes
-
-app.include_router(user_router, prefix="/user")
-app.include_router(event_router, prefix="/event")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
